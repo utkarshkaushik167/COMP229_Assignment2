75baf4a1940d4149eecb992b8ff5e3af2258ce9a
Utkarsh Kaushik <utkarshkaushik167@gmail.com>
2021-10-29 14:37:58 +0530

    atlas and heroku connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://utkarsh:F0L8VvHIgTXeeFs2@mongodbserver.cmafx.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
